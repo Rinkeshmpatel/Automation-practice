857c6fa18fde09dd1a2ea146196bf79bf701dc2b
Rinkeshmpatel <Rinkeshpm5@gmail.com>
2022-09-22 19:42:04 -0400

    automationpractice

---
+++ src/main/java/Pages/ProductPage.java
@@ -0,0 +1,121 @@
+package Pages;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utility.Utility;
+
+import java.util.List;
+
+public class ProductPage extends Utility {
+    private static final Logger log = LogManager.getLogger(ProductPage.class.getName());
+
+    public ProductPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+
+    @CacheLookup
+    @FindBy(xpath = "//a[@id='color_8']")
+    WebElement colorWht;
+
+    @CacheLookup
+    @FindBy(xpath = "//a[@id='color_13']")
+    WebElement colorOrg;
+
+    @CacheLookup
+    @FindBy(xpath = "//a[@id='color_15']")
+    WebElement colorGrn;
+
+    @CacheLookup
+    @FindBy(xpath = "//a[@id='color_14']")
+    WebElement colorBlu;
+
+    @CacheLookup
+    @FindBy(xpath = "//a[text()='Faded Short Sleeve T-shirts']")
+
+    WebElement fadedShort;
+    @CacheLookup
+    @FindBy(xpath = "//input[@id='quantity_wanted']")
+    WebElement inputboxQt;
+    @CacheLookup
+    @FindBy(xpath = "//select[@id='group_1']/option[1]")
+    WebElement small;
+
+    @CacheLookup
+    @FindBy(xpath = "//select[@id='group_1']/option[2]")
+    WebElement medium;
+
+    @CacheLookup
+    @FindBy(xpath = "//select[@id='group_1']/option[3]")
+    WebElement large;
+
+    @CacheLookup
+    @FindBy(css = "button[name='Submit'] span")
+    WebElement addToCartButton;
+
+    @CacheLookup
+    @FindBy(xpath = "//span[@title='Close window']")
+    WebElement close;
+
+
+    @CacheLookup
+    @FindBy (xpath = "//a[@class='product-name']")
+    List<WebElement> productlist;
+
+    public void clickOnTheProduct (String product) throws InterruptedException {
+        for (WebElement listPrd : productlist){
+            if (listPrd.getText().equalsIgnoreCase(product)){
+                Thread.sleep(2000);
+                listPrd.click();
+                break;
+            }
+        }
+    }
+    public void selColour(String color) {
+        if (color.equalsIgnoreCase("White")) {
+            clickOnElement(colorWht);
+
+        } else if (color.equalsIgnoreCase("Orange")) {
+            clickOnElement(colorOrg);
+
+        } else if (color.equalsIgnoreCase("Green")) {
+            clickOnElement(colorGrn);
+
+        } else if (color.equalsIgnoreCase("Blue")) {
+            clickOnElement(colorBlu);
+
+        }
+    }
+    public void clickOnFadedShort(){
+        clickOnElement(fadedShort);
+    }
+    public void enterQuantity(String value){
+        clearValue(inputboxQt);
+        sendTextToElement(inputboxQt,value);
+    }
+    public void selectYourComfortSize(String size) {
+        if (size.equalsIgnoreCase("s")) {
+            clickOnElement(small);
+
+        } else if (size.equalsIgnoreCase("m")) {
+            clickOnElement(medium);
+
+        } else if (size.equalsIgnoreCase("l")) {
+            clickOnElement(large);
+
+        }}
+    public void clickOnAddToCartButton () {
+        clickOnElement(addToCartButton);
+
+    }
+
+    public void clickToClose () {
+        clickOnElement(close);
+
+    }
+}
+
---
+++ src/test/java/com/automation/steps/WomenCategoryPageSteps.java
@@ -0,0 +1,66 @@
+package com.automation.steps;
+
+import Pages.HomePage;
+import Pages.SignInPage;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class WomenCategoryPageSteps {
+
+    @Given("^I am on Home Page$")
+    public void iAmOnHomePage() {
+    }
+
+    @When("^I Click on SignIn Link$")
+    public void iClickOnSignInLink() {
+        new HomePage().clickonsignin();
+    }
+
+    @Then("^I verify that \"([^\"]*)\" message is displayed$")
+    public void iVerifyThatMessageIsDisplayed(String autho) {
+        Assert.assertEquals(autho,new HomePage().verifytext());
+    }
+
+    @And("^I enter \"([^\"]*)\"$")
+    public void iEnter(String Username)  {
+        new SignInPage().enteremail(Username);
+    }
+
+    @And("^I Click on SignIn Button$")
+    public void iClickOnSignInButton() {
+        new SignInPage().clickonsigninbutton();
+    }
+
+    @Then("^I verify Error message \"([^\"]*)\"$")
+    public void iVerifyErrorMessage(String message) {
+        Assert.assertEquals(message,new SignInPage().verifyText(message));
+
+    }
+
+    @And("^I enter a \"([^\"]*)\"$")
+    public void iEnterA(String Password)  {
+        new SignInPage().enterpassword(Password);
+
+    }
+
+    @Then("^I verify Sign out link is display$")
+    public void iVerifySignOutLinkIsDisplay() {
+        String acctualresult = new SignInPage().verifysignout();
+        String expectedresult = "Sign out";
+        Assert.assertEquals(acctualresult,expectedresult);
+    }
+
+    @And("^I click on Signout Link$")
+    public void iClickOnSignoutLink() {
+        new SignInPage().clickonsignout();
+    }
+
+    @Then("^I verify that Signin Link is didplay$")
+    public void iVerifyThatSigninLinkIsDidplay() {
+        String acctualresult1 = new SignInPage().verifysigninlink();
+        String expectedresult1 = "Sign in";
+        Assert.assertEquals(acctualresult1,expectedresult1);
+    }
+}
